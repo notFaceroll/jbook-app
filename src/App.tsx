f12296d30bf6ef93f37bd0fa561f4d75a67e51ef
notFaceroll <overhealzlol@gmail.com>
2022-06-28 18:30:47 -0300

    feat: markdown editor This Installs react-md-editor package. Users can write .md files direct on the app and see the output right away; Add states to control the visibility of the md editor and user inputs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
 
 function App() {
   return (
     <div>
-      <CodeCell />
+      {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   );
 }
